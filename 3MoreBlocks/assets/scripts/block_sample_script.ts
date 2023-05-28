bab16115a2b9d587658c4f49a26a7a955cb81940
Dmitry Koba <55078980+Tvix57@users.noreply.github.com>
2023-05-28 22:36:06 +0700

    bloc script - 80% create - 20%

--- 3MoreBlocks/assets/scripts/block_sample_script.ts
+++ 3MoreBlocks/assets/scripts/block_sample_script.ts
@@ -9,50 +9,38 @@ export class block_sample_script extends Component {
     public widht: number = 50;
 
     private rigidbody: any;
-    private walk_force: number = 100;
+    private walk_force: number = 0.1;
 
-    constructor(height: number, widht: number) {
-        super();
-        this.height = height;
-        this.widht = widht;
-    }
-
-    onLoad() {
-        // выбор рандомного цвета
+    init() {
         const colors = [Color.RED, Color.GREEN, Color.BLUE];
         this.color = colors[Math.floor(Math.random() * colors.length)];
-        // создание примитивного квадрата
-        const rectangleNode = new Node('Rectangle');
-        this.node.addChild(rectangleNode);
-        const graphicsComponent = rectangleNode.addComponent(Graphics);
+
+        const graphicsComponent = this.node.addComponent(Graphics);
         graphicsComponent.rect(0, 0, this.height, this.widht);
         graphicsComponent.fillColor = this.color;
-        // привязка события
-        systemEvent.on(SystemEventType.MOUSE_UP, this.onClickEvent, this);
-    }
 
-    start() {
-        this.rigidbody = this.node.getComponent(RigidBody2D);
+       // systemEvent.on(SystemEventType.MOUSE_UP, this.onClickEvent, this);
+
+        this.rigidbody = this.node.addComponent(RigidBody2D);
     }
 
     update(deltaTime: number) {
-        this.rigidbody.applyForceCenter(new Vec2(0, -1 * this.walk_force), true);
+    
+       // this.rigidbody.applyForceCenter(new Vec2(0, -1 * this.walk_force), true); // error
     }
 
     onClickEvent() {
         let list: Node[] = [];
-        list = this.findNeighbours(list, this.color);
+        this.findNeighbours(list, this.color);
         if (list.length > 2) {
             list.forEach((child) => {
                 child.destroy();
             });
         }
-
     }
 
-    findNeighbours(list: Node[], start_color: Color) : Node[] {
+    findNeighbours(list: Node[], start_color: Color) {
 
-        // получить список 4 соседей.
         let closest_neighbour: Node[] = [];
 
         closest_neighbour.push(new Node);
@@ -61,24 +49,11 @@ export class block_sample_script extends Component {
         closest_neighbour.push(new Node);
 
         closest_neighbour.forEach((child) => {
-            if (child.getComponent(block_sample_script).color != start_color || list.indexOf(child) === -1) { // цвета не совпадают или данный сосед уже не добавлен
-                //remove node or not add in
+            if (child.getComponent(block_sample_script).color == start_color && list.indexOf(child) === -1) {
+                list.push(child);
+                child.getComponent(block_sample_script).findNeighbours(list, start_color);
             }
         });
-
-        if (closest_neighbour.length == 0) {
-            return null;
-        } else {
-
-    
-            list = list.concat(closest_neighbour);
-            ///вызвать функции поиска у соседей и проверить их на null
-            closest_neighbour.forEach((child) => {
-
-            });
-            return list;
-        }
-
     }
 }
 
--- 3MoreBlocks/assets/scripts/create_lvl_script.ts
+++ 3MoreBlocks/assets/scripts/create_lvl_script.ts
@@ -1,15 +1,31 @@
-import { _decorator, Component, Node, systemEvent, SystemEventType,  } from 'cc';
+import { _decorator, Component, Node, systemEvent, SystemEventType, Label, resources, Prefab, error, } from 'cc';
+import { block_sample_script } from '../scripts/block_sample_script';
 const { ccclass, property } = _decorator;
 
 @ccclass('create_lvl')
 export class create_lvl extends Component {
-    
+    @property({ type: Label })
+    private rowsLabel: Label | null = null;
+    @property({ type: Label })
+    private colsLabel: Label | null = null;
+    @property({ type: Node })
+    private nodeContainer: Node | null = null;
     start() {
-
+        this.createLvl(10, 16);
     }
 
-    update(deltaTime: number) {
-        
+    createLvl(rows: number, cols: number) {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                
+                var newNode = new Node;
+                const component = newNode.addComponent(block_sample_script);
+                component.height = 500;
+                component.widht = 500;
+                component.init();
+                this.nodeContainer.addChild(newNode);
+            }
+        }
     }
 }
 
